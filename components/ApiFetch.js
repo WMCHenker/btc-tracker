55c0e0d8f3422e5318677e7f439e88675bcf65ba
GIIIPFEL <kindlerherbert@gmail.com>
2022-02-22 11:31:56 +0100

    Comments added to ApiFetch.sj

--- components/ApiFetch.js
+++ components/ApiFetch.js
@@ -1,20 +1,25 @@
+// Added import of the NODE Library
 const CoinGecko = require('coingecko-api');
 
 function ApiFetch() {
 
-    //https://www.coingecko.com/en/api/documentation
-    //https://github.com/miscavage/CoinGecko-API
+    // https://www.coingecko.com/en/api/documentation
+    // https://github.com/miscavage/CoinGecko-API
 
-    //2. Initiate the CoinGecko API Client
+    // 2. Initiate the CoinGecko API Client
     const CoinGeckoClient = new CoinGecko();
 
-    //3. Make calls
+    // 3. Make API call and log it to the Console
     let fetchApi = async () => {
         let data = await CoinGeckoClient.coins.fetch('bitcoin', {});
         console.log(data);
     };
 
+    // 4. Call the function to get the Log
     fetchApi();
 }
 
 export default ApiFetch;
+
+// To add Later!
+// Return the data from the function and get the data on the index.js to gather and show the data
