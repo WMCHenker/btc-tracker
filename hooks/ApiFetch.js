1e484669fa21bb6baf2f7506113462f3377e3acd
GIIIPFEL <kindlerherbert@gmail.com>
2022-02-23 10:00:12 +0100

    nothing important, ignore that

--- hooks/ApiFetch.js
+++ hooks/ApiFetch.js
@@ -1,10 +1,10 @@
 // 1. Added import of the NODE Library
 const CoinGecko = require('coingecko-api');
 
-const ApiFetch = async () => {
-
     // https://www.coingecko.com/en/api/documentation
     // https://github.com/miscavage/CoinGecko-API
+
+const ApiFetch = async () => {
 
     // 2. Initiate the CoinGecko API Client
     const CoinGeckoClient = new CoinGecko();
