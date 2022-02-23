a3f86aef269b9018bccbfbd96cb59fa7d8d836b5
GIIIPFEL <kindlerherbert@gmail.com>
2022-02-23 20:03:14 +0100

    MySQL !!! Prisma used! Own new API!! Server Sided Rendering !!!!

---
+++ pages/api/Prisma.js
@@ -0,0 +1,23 @@
+const { PrismaClient } = require('@prisma/client')
+
+
+let prisma;
+
+// To fix an error, that you can't run PrismaClient in the Browser
+// if checks if you are a server
+if (typeof window === 'undefined') {
+  prisma = new PrismaClient()
+}
+
+async function prismaFetched() {
+  // SELECT * FROM bitcoin
+  let allBitcoinData = await prisma.bitcoin.findMany()
+  return allBitcoinData;
+}
+
+// API HANDLER
+export default function handler (req, res) {
+  prismaFetched().then(value => {
+    res.status(200).json(value);
+  })
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Head from 'next/head'
 import Main from '../components/Main';
 import styles from '../styles/Home.module.css'
+import { useEffect } from 'react';
 
-export default function Home() {
+export default function Home({ data }) {
+
+  useEffect(() => {
+    // Props working !
+    // MySQL log !!!!
+    console.log(data);
+  }, [])
+  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,4 +22,14 @@ export default function Home() {
       <Main />
     </div>
   )
+}
+
+// Server sided rendering (SSR)
+export async function getServerSideProps() {
+  // Fetch data from our own database API
+  const res = await fetch(`http://localhost:3000/api/Prisma`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
