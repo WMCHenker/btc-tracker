d620df2fd76e921e3fe34c0a31ccacb2b2407aa3
Lambda-Spg <Lambda-spg@protonmail.com>
2022-02-23 10:19:12 +0100

    Did some basic design

--- components/Main.js
+++ components/Main.js
@@ -19,26 +19,30 @@ const Main = () => {
 
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                {/* Bitcoin, becouse of the API output !!! WORKING */}
-                {fetched.name}
-            </h1>
-            <h2 className="text-2xl font-bold">
-                {/* ? Because when the JSX gets loaded the value of fetched is undefined/null. */}
-                {/* So the ? says the we don't know if it has any value */}
-                Currently {fetched?.market_data?.current_price.eur} €
-            </h2>
-            <ul>
-                <li>Highest of the last 24 hrs: {fetched?.market_data?.high_24h?.eur} €</li>
-                <li>Lowest of the last 24 hrs: {fetched?.market_data?.low_24h?.eur} €</li>
-                <li>Price change of the last 24 hrs: {fetched?.market_data?.price_change_24h_in_currency?.eur?.toFixed(2)} €</li>
-                <li>Price change of the last hr: {fetched?.market_data?.price_change_percentage_1h_in_currency?.eur?.toFixed(2)} €</li>
-                <li>Price change of the last 14d: {fetched?.market_data?.price_change_percentage_14d_in_currency?.eur?.toFixed(2)} €</li>
-                <li>Price change of the last 30d: {fetched?.market_data?.price_change_percentage_30d_in_currency?.eur?.toFixed(2)} €</li>
-                <li>Price change of the last 60d: {fetched?.market_data?.price_change_percentage_60d_in_currency?.eur?.toFixed(2)} €</li>
-                <li>Price change of the last 200d: {fetched?.market_data?.price_change_percentage_200d_in_currency?.eur?.toFixed(2)} €</li>
-                <li>Price change of the last 1y: {fetched?.market_data?.price_change_percentage_1y_in_currency?.eur?.toFixed(2)} €</li>
-            </ul>
+            <div className="h-screen w-screen flex items-center justify-center bg-slate-700">
+                <div className="mx-auto my-auto max-w-lg bg-slate-600 rounded-lg p-3">
+                <h1 className="text-3xl font-bold underline">
+                    {/* Bitcoin, becouse of the API output !!! WORKING */}
+                    {fetched.name}
+                </h1>
+                <h2 className="text-2xl font-bold">
+                    {/* ? Because when the JSX gets loaded the value of fetched is undefined/null. */}
+                    {/* So the ? says the we don't know if it has any value */}
+                    Currently {fetched?.market_data?.current_price.eur} €
+                </h2>
+                <ul>
+                    <li>Highest of the last 24 hrs: {fetched?.market_data?.high_24h?.eur} €</li>
+                    <li>Lowest of the last 24 hrs: {fetched?.market_data?.low_24h?.eur} €</li>
+                    <li>Price change of the last 24 hrs: {fetched?.market_data?.price_change_24h_in_currency?.eur?.toFixed(2)} €</li>
+                    <li>Price change of the last hr: {fetched?.market_data?.price_change_percentage_1h_in_currency?.eur?.toFixed(2)} €</li>
+                    <li>Price change of the last 14d: {fetched?.market_data?.price_change_percentage_14d_in_currency?.eur?.toFixed(2)} €</li>
+                    <li>Price change of the last 30d: {fetched?.market_data?.price_change_percentage_30d_in_currency?.eur?.toFixed(2)} €</li>
+                    <li>Price change of the last 60d: {fetched?.market_data?.price_change_percentage_60d_in_currency?.eur?.toFixed(2)} €</li>
+                    <li>Price change of the last 200d: {fetched?.market_data?.price_change_percentage_200d_in_currency?.eur?.toFixed(2)} €</li>
+                    <li>Price change of the last 1y: {fetched?.market_data?.price_change_percentage_1y_in_currency?.eur?.toFixed(2)} €</li>
+                </ul>
+                </div>
+            </div>
         </>
     )
 }
