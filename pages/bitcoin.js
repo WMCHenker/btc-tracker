cd5d38b87a4f8c4dc16f9bcba43ae8e76fd5e826
GIIIPFEL <kindlerherbert@gmail.com>
2022-03-01 11:51:44 +0100

    Auto updating API on the chart

--- pages/bitcoin.js
+++ pages/bitcoin.js
@@ -1,26 +1,53 @@
 import Navbar from "../components/Navbar";
 import { BitcoinChart } from "../components/BitcoinChart";
+import { useEffect, useState } from "react";
+import LoadingPage from "../components/LoadingPage";
 
 export default function Bitcoin({ data }) {
-  return (
-    <>
-        <Navbar></Navbar>
-        <div className="pt-56 mx-auto max-w-6xl text-center">
-            <h1 className="font-bold text-2xl">Bitcoin</h1>
-            <h2 className="text-xl">{data[data?.length - 1].current_value} €</h2>
-            <BitcoinChart apiData={data}/>
-        </div>
-    </>
-  )
+  const [dbData, setDbData] = useState();
+
+  useEffect(() => {
+    // Props working !
+    // MySQL log !!!!
+    console.log(data);
+    setDbData(data);
+  }, [])
+
+  useEffect(() => {
+    // Props working !
+    // MySQL log !!!!
+    setTimeout(() => {
+      const fetchApiAgain = async () => {
+        const res = await fetch(`http://localhost:3000/api/Prisma`)
+        const fetchedData = await res.json()
+        setDbData(fetchedData);
+      };
+      fetchApiAgain();
+    }, 30000);
+  })
+
+  if (dbData) {
+    return (
+      <>
+          <Navbar></Navbar>
+          <div className="pt-56 mx-auto max-w-6xl text-center">
+              <h1 className="font-bold text-2xl">Bitcoin</h1>
+              <h2 className="text-xl">{dbData[data?.length - 1].current_value} €</h2>
+              <BitcoinChart apiData={dbData}/>
+          </div>
+      </>
+    )
+  } else {
+    return <LoadingPage />
+  }
 }
 
-
+// Server sided rendering (SSR)
 export async function getServerSideProps() {
-    // Fetch data from our own database API
-    const res = await fetch(`http://localhost:3000/api/Prisma`)
-    const data = await res.json()
-  
-    // Pass data to the page via props
-    return { props: { data } }
-  }
-  
+  // Fetch data from our own database API
+  const res = await fetch(`http://localhost:3000/api/Prisma`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
